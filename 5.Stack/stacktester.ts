79df398773da9a23c874504c96edc357d6026f31
Fulltilt Coding <118578711+FulltiltCoding@users.noreply.github.com>
2023-01-04 14:56:46 -0500

    Update stacktester.ts

--- 5.Stack/stacktester.ts
+++ 5.Stack/stacktester.ts
@@ -1,29 +1,36 @@
-// main entry point
+// tsc --target esnext stacktester.ts && node stacktester.js && rm *.js
+
 import { Stack } from "./stack.js";
 
-const main = (): void => {
-
-    const stack = new Stack<number>();
-    console.log("isEmpty", stack.isEmpty());
-
-    for (let i: number = 0; i < 30; i++) {
-        stack.push(i);
-    }
-
-    console.log("size", stack.size());
-    stack.print();
-    console.log(stack.pop());
-    console.log(stack.pop());
-    console.log(stack.pop());
-    console.log(stack.pop());
-    console.log(stack.pop());
-    console.log(stack.pop());
-    console.log("size", stack.size());
-    console.log("isEmpty", stack.isEmpty());
-    stack.print();
-    const stack2 = stack.clone();
-    console.log("==============================");
-    stack2.print();
+const random = (min, max): number => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
-main();
+// create the stack
+const stack: Stack<number> = new Stack<number>();
+const size: number = 20;
+
+// fill the stack
+for (let i: number = 0; i < size; i++) {
+    stack.push(random(1, 100));
+}
+
+// print the stack
+console.log("Stack 1============================")
+stack.print();
+console.log("size", stack.size());
+console.log("isEmpty", stack.isEmpty());
+
+console.table(stack.toArray());
+
+// empty
+for (let i: number = 0; i < size; i++) {
+    console.log("pop():", stack.pop());
+}
+
+console.log("Stack 1==============================");
+console.log("size", stack.size());
+console.log("isEmpty", stack.isEmpty());
+stack.print();
